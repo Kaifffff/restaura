09de2ab6b236765d19428be9aa0d405e790c5983
kaif <shaikhkaif3258@gmail.com>
2025-01-19 23:55:47 +0530

    created dishe section

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import HeroSection from './components/HeroSection'
 import Navbar from './components/Navbar'
-import DishCard from './components/DishCard'
+import Dishes from './components/Dishes'
 
 export default function App() {
   return (
     <main className='overflow-y-hidden text-neutral-200 antialiased'>
       <HeroSection/>
       <Navbar/>
-      <DishCard/>
+      <Dishes/>
     </main>
   )
 }
