18cd470cca8105aed935d745c3e427d195b26747
kaif <shaikhkaif3258@gmail.com>
2025-01-19 23:46:43 +0530

    created dishcard section

--- src/components/DishCard.jsx
+++ src/components/DishCard.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
 
-export default function DishCard() {
+export default function DishCard({project}) {
   return (
-    <div>DishCard</div>
+    <div>
+        <img src={project.image}
+        alt={project.title} 
+        className='rounded-3xl p-2'/>
+        <div className='p-4'>
+            <h3 className='mb-2 text-2xl font-bold tracking-tighter'>
+                {project.title}
+            </h3>
+            <p className='text-sm'>{project.description}</p>
+        </div>
+    </div>
   )
 }
