cb1d46683c5ee8559018d7bbfff17d3b09503acd
kaif <shaikhkaif3258@gmail.com>
2025-01-19 23:28:17 +0530

    restaura

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import HeroSection from './components/HeroSection'
+import Navbar from './components/Navbar'
+
+export default function App() {
+  return (
+    <main className='overflow-y-hidden text-neutral-200 antialiased'>
+      <HeroSection/>
+      <Navbar/>
+    </main>
+  )
+}
