90b081bc08b21512860a0aaf56c2940d75e63945
kaif <shaikhkaif3258@gmail.com>
2025-01-20 15:46:21 +0530

    added motion to expertise section

--- src/components/Expertise.jsx
+++ src/components/Expertise.jsx
@@ -1,17 +1,40 @@
 import React from "react";
 import { CUSINES } from "../constants";
+import { motion } from "framer-motion";
 
+const containerVariants = {
+  hidden : {opacity : 0},
+  show: {
+    opacity:1,
+    transition:{
+      staggerChildren:1,
+    }
+  }
+}
+
+const ItemVariants = {
+  hidden:{opacity:0 , y:20},
+  show:{opacity:1,y:0,transition:{
+    duration:0.8
+  }}
+}
 export default function Expertise() {
   return (
     <section id="expertise">
       <h2 className="my-8 text-center text-3xl tracking-tighter lg:text-4xl">
         Our Expertise
       </h2>
-      <div className="container mx-auto px-4">
+      <motion.div
+      initial='hidden'
+      whileInView='show'
+      variants={containerVariants}
+       className="container mx-auto px-4">
         {CUSINES.map((cusine, index) => (
-          <div
+          <motion.div
+
             key={index}
             className="flex items-center border-b-4 border-dotted border-neutral-700/40 py-2"
+            variants={ItemVariants}
           >
             <div className="flex-shrink-0 pr-8 text-2xl">{cusine.number}</div>
             <div className="w-1/3 flex-shrink-0">
@@ -24,9 +47,9 @@ export default function Expertise() {
                 <h3 className="text-2xl uppercase tracking-tighter text-rose-300">
                     { cusine.title}</h3>
                     <p className="mt-4 text-lg tracking-tighter"> {cusine.description}</p> </div>
-          </div>
+          </motion.div>
         ))}
-      </div>
+      </motion.div>
     </section>
   )
 }
